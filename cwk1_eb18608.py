5a5123b1bd2071a3b03a351122d7e086d19d6d74
eb18608 <eb18608@bristol.ac.uk>
2020-03-23 14:59:26 +0000

    Blackboard being annoying

--- cwk1_eb18608.py
+++ cwk1_eb18608.py
@@ -102,6 +102,39 @@ def inputOutput():
         print("Enter a valid file format: filename.format")
         exit()
 
+def view_data_segments(xs, ys, y_final_plot, finalPowerSet, filename):
+    """Visualises the input file with each segment plotted in a different colour.
+    Args:
+        xs : List/array-like of x co-ordinates.
+        ys : List/array-like of y co-ordinates.
+    Returns:
+        None
+    """
+    assert len(xs) == len(ys)
+    assert len(xs) % 20 == 0
+    len_data = len(xs)
+    num_segments = len_data // 20
+    plot = plt.subplot()
+    colour = np.concatenate([[i] * 20 for i in range(num_segments)])
+    colourSegment = [0, 1, 2, 3, 4, 5]
+    plt.set_cmap('Dark2')
+
+    data = plot.scatter(xs, ys, c=colour)
+    legend1 = plot.legend(*data.legend_elements(),
+                        loc="upper right", title="Data segment", fontsize='small', title_fontsize='small')
+    plot.add_artist(legend1)
+    colourSegment = [0, 1, 2, 3, 4, 5]
+    for l in range(num_segments):
+        # print(l)
+        start = l*20
+        end = start + 20
+        # print("end:", end)
+        plt.set_cmap('Dark2')
+        line = plot.plot(xs[start: end], y_final_plot[l].T, label='Regression Line:' +str(l))
+
+    plot.legend(loc=2, fontsize='small', title_fontsize='small')
+    # plt.savefig('Plots/'+filename[11:-4], dpi = 400)
+    plt.show()
 
 def main():
 
@@ -181,6 +214,6 @@ def main():
 
     print(totalError)
     if(plot == True):
-        utilities.view_data_segments(xs , ys, y_final_plot, finalPowerSet, fileName)
+        view_data_segments(xs , ys, y_final_plot, finalPowerSet, fileName)
 
 main()
